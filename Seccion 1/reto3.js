cc510c77a857d8d5f8c190cba9a7d92d5ae5df09
Mahi_G13 <maicolgallego14@gmail.com>
2024-04-19 15:59:40 -0500

    WIP: resolving again challenge 3, new correptions

--- Seccion 1/reto3.js
+++ Seccion 1/reto3.js
@@ -1,14 +1,22 @@
+// Variables
+
 let budgetUser;
 let costHome;
 let costTransport;
 let costFood;
 let tripDuration;
 let umbralAllowedForExtraSpends;
+let askRegisterProduct;
+let nameProduct;
+let priceProduct;
+let listProducts = [];
 
-function askWithNanVerify(message) {
+// Funciones
+
+function askTypeNumberWithNanVerify(message) {
   let dato = NaN;
   while (isNaN(dato) || !dato) {
-    dato = Number(prompt(message));
+    dato = Number(prompt(message).trim());
     // console.log(Boolean(dato));
     if (isNaN(dato) || !dato) {
       alert('Por favor completa con un valor numerico');
@@ -18,16 +26,39 @@ function askWithNanVerify(message) {
   }
 }
 
-budgetUser = askWithNanVerify('El total de su presupuesto inicial');
-costHome = askWithNanVerify('Costo alojamiento por dia');
-costTransport = askWithNanVerify('Costo transporte por dia');
-costFood = askWithNanVerify('Costo de alimentacion por dia');
-tripDuration = askWithNanVerify('Dias de duracion del viaje');
+function askTypeString(message){
+
+  let dato;
+
+  do{
+    dato = prompt(message).trim();
+  }while(dato !== "")
+
+  return dato;
+}
+
+function constructorAndPushProduct(name, price,list){
+  const product = {
+    name: name,
+    price: price
+  }
+
+  list.shift(product)
+
+  return list
+
+}
+
+budgetUser = askTypeNumberWithNanVerify('El total de su presupuesto inicial');
+costHome = askTypeNumberWithNanVerify('Costo alojamiento por dia');
+costTransport = askTypeNumberWithNanVerify('Costo transporte por dia');
+costFood = askTypeNumberWithNanVerify('Costo de alimentacion por dia');
+tripDuration = askTypeNumberWithNanVerify('Dias de duracion del viaje');
 
 budgetUser = budgetUser - (costHome + costTransport + costFood) * tripDuration;
 
 if (budgetUser > 0) {
-  umbralAllowedForExtraSpends = askWithNanVerify(
+  umbralAllowedForExtraSpends = askTypeNumberWithNanVerify(
     'Cual es tu umbral mínimo necesario para gastos imprevistos o emergencias'
   );
   alert(
@@ -36,6 +67,45 @@ if (budgetUser > 0) {
 
   if (budgetUser >= umbralAllowedForExtraSpends) {
     alert('Estas en rango para considerar realizar una compra extra');
+    
+    askRegisterProduct = confirm("Quieres registrar los productos que pretendes comprar")
+    
+    if(askRegisterProduct){
+      while (askRegisterProduct) {
+        nameProduct = askTypeStringWithNanVerify('Nombre del producto')
+        priceProduct = askTypeNumberWithNanVerify('Precio del producto')
+        
+        listProducts = constructorAndPushProduct(nameProduct, priceProduct, listProduct)
+
+        if(priceProduct <= umbralAllowedForExtraSpends){
+          listProducts = constructorAndPushProduct(nameProduct, priceProduct, listProduct)
+          alert("Registro exitoso")
+        }else{
+          alert("El producto supera tu capacidad. No se registro")
+        }
+        askRegisterProduct = confirm("¿Deseas registrar mas productos?")
+      }
+
+      if(listProducts[0]){
+        const cheapestProduct = listProducts.reduce((cheapest, currentProduct) => {
+          if(currentProduct.price < cheapest.price){
+            cheapest = currentProduct;
+          }
+          return cheapest;
+        }, {name: "falso", price: 1000000});
+
+        alert(`Te recomiendo comprar el producto mas barato\nProducto: ${cheapestProduct.name}\nPrecio: $${cheapestProduct.price}\n\nUn gusto ayudarte. Adios`)
+        
+      }
+      else{
+        alert("Lo siento no has registrado nigun producto. Adios")
+      }
+    }
+
+    else{
+      alert("Un placer ayudarte. Que tengas buen dia. Adios")
+    }
+
   } else {
     alert(
       'Estas muy justo evite gastos adicionales para no sobrepasar su presupuesto'
